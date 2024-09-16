afe027644602eb90d5ee4e085b1b7336b212a07d
AndreyTara <explorituse@gmail.com>
2024-09-17 01:55:06 +0300

    'init-simple-server'

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+export const startServer = () => {
+  const app = express();
+
+  const PORT = 3000;
+
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Hello world",
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
